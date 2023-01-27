48d22b3901089b423d35c2c687a7a7a8e32db361
Dhyanesh Panchal <dhynincoding2004@gmail.com>
2023-01-28 00:25:33 +0530

    Deployment in github pages

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
+ReactDOM.render(<App />, document.getElementById("root"))
